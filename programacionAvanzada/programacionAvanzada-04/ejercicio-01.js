80d3164edbe0898cc3671e3e64c706f9c9267a64
jorgegomezgerling <jorgegomezgerling@gmail.com>
2024-10-05 18:12:11 -0300

    primer ejecicio frutas pop, push

---
+++ programacionAvanzada/programacionAvanzada-04/ejercicio-01.js
@@ -0,0 +1,13 @@
+// 1. Agregar y Eliminar Elementos:
+// ○ Crea un array frutas con los valores ["manzana", "banana", "pera"]. Usa
+// push para agregar una fruta al final, y pop para eliminar la última
+// fruta.
+
+frutas = ["manzana", "banana", "pera"];
+
+
+frutas.pop();
+frutas.push("sal muera");
+
+console.log(frutas);
+
